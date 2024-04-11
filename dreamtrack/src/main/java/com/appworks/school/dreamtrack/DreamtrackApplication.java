d3be45438002751bc38ddd4ddefa9c988ccb4838
phoebe <mongzhen0856@gmail.com>
2024-04-11 13:03:23 +0800

    Initial

---
+++ dreamtrack/src/main/java/com/appworks/school/dreamtrack/DreamtrackApplication.java
@@ -0,0 +1,13 @@
+package com.appworks.school.dreamtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamtrackApplication.class, args);
+	}
+
+}
